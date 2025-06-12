91960dbdd274c9d35f8e6e126f781e281f370c42
Ashikul Islam <ashikulislamfb@gmail.com>
2025-06-12 21:43:32 +0600

    react icon library

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import SearchTask from './SearchTask';
 import TaskActions from './TaskActions';
 import TaskList from './TaskList';
 
 export default function TastBoard() {
+    const defaultData = {
+        id: crypto.randomUUID(),
+        title: 'Integration API',
+        description:
+            'Connect an existing API into a third-party database using secure methods and handle data exchange efficiently',
+        tags: ['web', 'python', 'API'],
+        priority: 'High',
+        isFavourite: true,
+    };
+    const [tasks, setTasks] = useState([defaultData]);
     return (
         <section className="mb-20" id="tasks">
             <div className="container">
@@ -11,7 +22,7 @@ export default function TastBoard() {
                 </div>
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#0A192C] px-6 py-8 md:px-9 md:py-16">
                     <TaskActions />
-                    <TaskList />
+                    <TaskList tasks={tasks} />
                 </div>
             </div>
         </section>
