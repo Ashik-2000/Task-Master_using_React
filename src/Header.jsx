05319a857cc7ec3368ffc1e4486e4cb0dd63940f
Ashikul Islam <ashikulislamfb@gmail.com>
2025-07-07 17:24:28 +0600

    refined

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,12 @@
-import LogoSVG from './assets/lws-logo-en.svg';
-
 export default function Header() {
     return (
         <nav className="fixed top-0 z-50 w-full !bg-[#0A192F] py-6 md:py-8">
             <div className="container mx-auto flex items-center justify-between gap-x-6">
-                <a href="/">
-                    <img className="h-[45px]" src={LogoSVG} alt="Lws" />
+                <a
+                    href="/"
+                    className="font-serif text-2xl font-bold text-white"
+                >
+                    Task Master
                 </a>
             </div>
         </nav>
